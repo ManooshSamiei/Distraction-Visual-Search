867a494c9c9c9d4d0231ffe0f8564f746ac0a91e
Manoosh Samiei <manoosh.samiei@gmail.com>
2022-03-09 23:22:05 -0500

    renamed saliency metric computation file

--- compute_sal_metrics.sh
+++ compute_sal_metrics.sh
@@ -2,7 +2,7 @@ DIR="/nas/EOS/users/manoosh/sal/"
 PYSAL=False
 #docker run --gpus device=5 --rm -u $(id -u):$(id -g) -v $(pwd):/workspace \
 #    -v /mnt:/mnt -v /media:/media -v /srv:/srv -v /nas:/nas -p 6967:6967 eos/tf1.15-conda:latest \
-python /nas/EOS/users/manoosh/sal/Predicting-Salience-During-Visual-Search/saliency_metrics.py \
+python /nas/EOS/users/manoosh/sal/Predicting-Salience-During-Visual-Search/compute_saliency_metrics.py \
     --path=$DIR \
     --use-pysaliency=$PYSAL
 
