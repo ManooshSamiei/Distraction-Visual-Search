9ffa4f5070b2f3cdd0756afcc50d6218cd64cb49
manoosh.samiei <manoosh.samiei@gmail.com>
2022-03-12 23:59:12 -0500

    bash file updated

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
-# rm -r /nas/EOS/users/manoosh/sal/cocosearch/*
-# rm -r /nas/EOS/users/manoosh/sal/results/*
+rm -r /nas/EOS/users/manoosh/sal/cocosearch/*
+rm -r /nas/EOS/users/manoosh/sal/results/*
 
 DOWNLOAD_DIR="/nas/EOS/users/manoosh/sal"
 
@@ -7,12 +7,12 @@ SIGMA=11
 DATA_DIR="/nas/EOS/users/manoosh/sal/cocosearch"
 AVAILABLE_GPU="0"
 ##preprocess data
-# docker run --gpus all -e CUDA_VISIBLE_DEVICES=$AVAILABLE_GPU --rm -u $(id -u):$(id -g) -v $(pwd):/workspace \
-#     -v /mnt:/mnt -v /media:/media -v /srv:/srv -v /nas:/nas -p 7967:7967 eos/tf1.15-conda:latest_ss \
-#     python /nas/EOS/users/manoosh/sal/Predicting-Salience-During-Visual-Search/data_preprocessing.py \
-#     --dldir=$DOWNLOAD_DIR \
-#     --sigma=$SIGMA \
-#     --datadir=$DATA_DIR \
+docker run --gpus all -e CUDA_VISIBLE_DEVICES=$AVAILABLE_GPU --rm -u $(id -u):$(id -g) -v $(pwd):/workspace \
+    -v /mnt:/mnt -v /media:/media -v /srv:/srv -v /nas:/nas -p 7967:7967 eos/tf1.15-conda:latest_ss \
+    python /nas/EOS/users/manoosh/sal/Predicting-Salience-During-Visual-Search/data_preprocessing.py \
+    --dldir=$DOWNLOAD_DIR \
+    --sigma=$SIGMA \
+    --datadir=$DATA_DIR \
 
 ##train model
 
@@ -20,23 +20,23 @@ DIR="/nas/EOS/users/manoosh/sal/"
 PHASE="train"
 THRESHOLD=30
 
-# docker run --gpus all -e CUDA_VISIBLE_DEVICES=$AVAILABLE_GPU --rm -u $(id -u):$(id -g) -v $(pwd):/workspace \
-#     -v /mnt:/mnt -v /media:/media -v /srv:/srv -v /nas:/nas -p 7967:7967 eos/tf1.15-conda:latest_ss \
-#     python /nas/EOS/users/manoosh/sal/Predicting-Salience-During-Visual-Search/main.py \
-#         --path=$DIR \
-#         --phase=$PHASE \
-#         --threshold=$THRESHOLD
+docker run --gpus all -e CUDA_VISIBLE_DEVICES=$AVAILABLE_GPU --rm -u $(id -u):$(id -g) -v $(pwd):/workspace \
+    -v /mnt:/mnt -v /media:/media -v /srv:/srv -v /nas:/nas -p 7967:7967 eos/tf1.15-conda:latest_ss \
+    python /nas/EOS/users/manoosh/sal/Predicting-Salience-During-Visual-Search/main.py \
+        --path=$DIR \
+        --phase=$PHASE \
+        --threshold=$THRESHOLD
 
 ##test model
 
 PHASE_1="test"
 
-# docker run --gpus all -e CUDA_VISIBLE_DEVICES=$AVAILABLE_GPU --rm -u $(id -u):$(id -g) -v $(pwd):/workspace \
-#     -v /mnt:/mnt -v /media:/media -v /srv:/srv -v /nas:/nas -p 7967:7967 eos/tf1.15-conda:latest_ss \
-#     python /nas/EOS/users/manoosh/sal/Predicting-Salience-During-Visual-Search/main.py \
-#         --path=$DIR \
-#         --phase=$PHASE_1 \
-#         --threshold=$THRESHOLD
+docker run --gpus all -e CUDA_VISIBLE_DEVICES=$AVAILABLE_GPU --rm -u $(id -u):$(id -g) -v $(pwd):/workspace \
+    -v /mnt:/mnt -v /media:/media -v /srv:/srv -v /nas:/nas -p 7967:7967 eos/tf1.15-conda:latest_ss \
+    python /nas/EOS/users/manoosh/sal/Predicting-Salience-During-Visual-Search/main.py \
+        --path=$DIR \
+        --phase=$PHASE_1 \
+        --threshold=$THRESHOLD
 
 ##compute saliency metrics
 
