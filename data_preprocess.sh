138167284442b100bb805d7ea77a0a03fa8e9a9a
manoosh.samiei <manoosh.samiei@gmail.com>
2022-03-18 00:43:33 -0400

    removed unneccessary bash files

--- data_preprocess.sh
+++
@@ -1,9 +0,0 @@
-DOWNLOAD_DIR="/nas/EOS/users/manoosh/sal"
-SIGMA=11
-DATA_DIR="/nas/EOS/users/manoosh/sal/cocosearch"
-
-python /nas/EOS/users/manoosh/sal/Predicting-Salience-During-Visual-Search/data_preprocessing.py \
-    --dldir=$DOWNLOAD_DIR \
-    --sigma=$SIGMA \
-    --datadir=$DATA_DIR \
-
