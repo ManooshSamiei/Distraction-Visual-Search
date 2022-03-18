93ec6586d5d83100d4e536c8c0018bfb7c0cfd7c
manoosh.samiei <manoosh.samiei@gmail.com>
2022-03-18 00:46:58 -0400

    small updates to the bash file

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,6 @@ rm -r /nas/EOS/users/manoosh/sal/cocosearch/*
 rm -r /nas/EOS/users/manoosh/sal/results/*
 
 DOWNLOAD_DIR="/nas/EOS/users/manoosh/sal"
-
 SIGMA=11
 DATA_DIR="/nas/EOS/users/manoosh/sal/cocosearch"
 AVAILABLE_GPU="0"
@@ -31,17 +30,20 @@ docker run --gpus all -e CUDA_VISIBLE_DEVICES=$AVAILABLE_GPU --rm -u $(id -u):$(
 
 # ##test model
 
-PHASE_1="test"
+DIR="/nas/EOS/users/manoosh/sal/"
+PHASE_t="test"
+THRESHOLD=30
 
 docker run --gpus all -e CUDA_VISIBLE_DEVICES=$AVAILABLE_GPU --rm -u $(id -u):$(id -g) -v $(pwd):/workspace \
     -v /mnt:/mnt -v /media:/media -v /srv:/srv -v /nas:/nas -p 7967:7967 eos/tf1.15-conda:latest_ss \
     python /nas/EOS/users/manoosh/sal/Predicting-Salience-During-Visual-Search/main.py \
         --path=$DIR \
-        --phase=$PHASE_1 \
+        --phase=$PHASE_t \
         --threshold=$THRESHOLD
 
 # ##compute saliency metrics
 
+DIR="/nas/EOS/users/manoosh/sal/"
 PYSAL=False
 CSV_DIR=${DOWNLOAD_DIR}/"results/"
 
